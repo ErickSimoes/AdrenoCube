92c82eeb358bbdb2071eb8a10f5cfb4bf1e14562
Erick Simões <erick.simoes@live.com>
2017-02-08 15:53:38 -0300

    Create Cube

--- app/src/main/java/model/Cubo.java
+++ app/src/main/java/model/Cubo.java
@@ -1,8 +1,27 @@
 package model;
 
-/**
- * Created by erick on 08/02/2017.
- */
-
 public class Cubo {
+    private static Cubo instance = null;
+
+    public static Cubo getInstance() {
+        if (instance == null) {
+            instance = new Cubo();
+        }
+        return instance;
+    }
+
+    final float[] cubePositionData={
+            // Face frontal
+            -1.0f, 1.0f, 1.0f,-1.0f, -1.0f, 1.0f,1.0f, 1.0f, 1.0f,-1.0f, -1.0f, 1.0f,1.0f, -1.0f, 1.0f,1.0f, 1.0f, 1.0f,
+            // Face direita
+            1.0f, 1.0f, 1.0f,1.0f, -1.0f, 1.0f,1.0f, 1.0f, -1.0f,1.0f, -1.0f, 1.0f,1.0f, -1.0f, -1.0f,1.0f, 1.0f, -1.0f,
+            // Face traseira
+            1.0f, 1.0f, -1.0f,1.0f, -1.0f, -1.0f,-1.0f, 1.0f, -1.0f,1.0f, -1.0f, -1.0f,-1.0f, -1.0f, -1.0f,-1.0f, 1.0f, -1.0f,
+            // Face esquerda
+            -1.0f, 1.0f, -1.0f,-1.0f, -1.0f, -1.0f,-1.0f, 1.0f, 1.0f,-1.0f, -1.0f, -1.0f,-1.0f, -1.0f, 1.0f,-1.0f, 1.0f, 1.0f,
+            // Face superior
+            -1.0f, 1.0f, -1.0f,-1.0f, 1.0f, 1.0f,1.0f, 1.0f, -1.0f,-1.0f, 1.0f, 1.0f,1.0f, 1.0f, 1.0f,1.0f, 1.0f, -1.0f,
+            // Face inferior
+            1.0f, -1.0f, -1.0f,1.0f, -1.0f, 1.0f,-1.0f, -1.0f, -1.0f,1.0f, -1.0f, 1.0f,-1.0f, -1.0f, 1.0f,-1.0f, -1.0f, -1.0f,
+    };
 }
