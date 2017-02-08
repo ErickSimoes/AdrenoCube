a23c04647bac06168dd4504390905ecc85963e40
Erick Simões <erick.simoes@live.com>
2017-02-08 15:34:21 -0300

    Define GLSurfaceView

--- app/src/main/java/com/erick/adrenocube/AdrenoActivity.java
+++ app/src/main/java/com/erick/adrenocube/AdrenoActivity.java
@@ -1,13 +1,19 @@
 package com.erick.adrenocube;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AdrenoActivity extends AppCompatActivity {
 
+    private GLSurfaceView glSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        glSurfaceView = new GLSurfaceView(this);
+
         setContentView(R.layout.activity_adreno);
     }
 }
